95384b96cc3df94fe7d1ec7f9b8228b054683b16
seo2im <seo2im6492@gmail.com>
2022-05-27 18:51:21 +0900

    fix: fix fetcher header

--- client/app/src/api/fetcher.tsx
+++ client/app/src/api/fetcher.tsx
@@ -3,7 +3,6 @@ import { BASEURL } from "../env"
 export const fetcher = async (path: string, headers: any, options: any) => {
   return await fetch(`${BASEURL}${path}`, {
     headers: {
-      'Access-Control-Allow-Headers': 'Authorization',
       ...headers
     },
     ...options
