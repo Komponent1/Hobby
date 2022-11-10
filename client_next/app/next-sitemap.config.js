4f1afaf890e620d9b3ffd586cb69c4a89bf8e4ea
seo2im <seo2im6492@gmail.com>
2022-11-10 12:04:25 +0900

    🚑 sitemap 관련 오류 픽스(경로)

--- client_next/app/next-sitemap.config.js
+++ client_next/app/next-sitemap.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://blog-sage-pi.vercel.app/',
+  siteUrl: process.env.SITE_URL || 'https://blog-sage-pi.vercel.app',
   generateRobotsTxt: true, // (optional)
   sitemapSize: 7000,
   exclude: ['/server-sitemap-index.xml'], // <= exclude here
   robotsTxtOptions: {
     additionalSitemaps: [
-      `${process.env.SITE_URL}/server-sitemap-index.xml`, // <==== Add here
+      `${process.env.SITE_URL}server-sitemap-index.xml`, // <==== Add here
     ],
   },
 }
