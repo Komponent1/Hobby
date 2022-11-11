05dd2e8a4fa3ae9dac58ba7b4e94743052db2b0f
seo2im <seo2im6492@gmail.com>
2022-11-11 18:38:26 +0900

    Squashed commit of the following:
    commit 74e6b68a06964d32ae8da0a779dc18f7745ec48e
    Author: seo2im <seo2im6492@gmail.com>
    Date:   Fri Nov 11 18:37:45 2022 +0900
    
        🐛 잘못된 사이트맵 수정

--- client_next/app/pages/server-sitemap-index.xml/index.tsx
+++ client_next/app/pages/server-sitemap-index.xml/index.tsx
@@ -1,13 +1,20 @@
 // pages/server-sitemap-index.xml/index.tsx
-import { getServerSideSitemapIndex } from 'next-sitemap';
+import { getServerSideSitemap } from 'next-sitemap';
 import { GetServerSideProps } from 'next';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { articles } = await fetch(`${process.env.BASEURL}/api/articles`).then((res) => res.json());
+  const lastmod = new Date().toISOString();
+  const field = articles.map(({ id }: { id: number }) => ({
+    loc: `${process.env.SITE_URL}/article/${id}`,
+    changefreq: 'daily',
+    priority: '1.0',
+    lastmod,
+  }));
 
-  return getServerSideSitemapIndex(
+  return getServerSideSitemap(
     ctx,
-    articles.map(({ id }: { id: number }) => `${process.env.SITE_URL}/article/${id}`),
+    field,
   );
 };
 
