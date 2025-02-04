c0a8828682113e986639ba0a12fe831b4d2cc045
seo2im <seolim6492@gmail.com>
2025-02-05 01:27:18 +0900

    fix: fix image position and svg error tag

--- client_next/app/srcs/steam/components/steam.components.pagination.tsx
+++ client_next/app/srcs/steam/components/steam.components.pagination.tsx
@@ -88,7 +88,7 @@ const Pagenation: React.FC<Props> = ({
           >
             <span className="sr-only">Next</span>
             <svg className="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
             </svg>
           </button>
         </li>
