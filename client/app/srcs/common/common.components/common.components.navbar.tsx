7fdb18d1e222b1542b0d93531fc97700cc9c13ef
seo2im <seolim6492@gmail.com>
2025-02-11 18:13:56 +0900

    feature: add load page for ux

--- client/app/srcs/common/common.components/common.components.navbar.tsx
+++ client/app/srcs/common/common.components/common.components.navbar.tsx
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
+import { useRouter } from "next/router";
 import pages from '../../page.config.json';
 import {PageConfig} from '../common.dto';
+import Portal from "./common.components.portal";
+import LoadPage from "./common.components.loadPage";
 
-const Navbar: React.FC = () => (
-  <nav className="bg-slate-900 fixed w-full z-50 top-0 start-0 shadow-md">
-    <div className="max-w-(--breakpoint-xl) flex flex-wrap items-center justify-between mx-auto p-4">
-      <div className="flex items-center space-x-3 rtl:space-x-reverse">
-        <Image src="/logo.png" className="h-8" alt="Flowbite Logo" width={32} height={32} />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">SEOLIM</span>
+const Navbar: React.FC = () => {
+  const router = useRouter();
+  const [isLoad, setIsLoad] = useState<boolean>(false);
+  const onLink = useCallback((page: keyof PageConfig) => {
+    if ((pages as PageConfig)[page].path === router.route) return;
+    setIsLoad(true);
+    router.push((pages as PageConfig)[page].path);
+  }, [router]);
+
+  return (
+    <nav className="bg-slate-900 fixed w-full z-50 top-0 start-0 shadow-md">
+      <div className="max-w-(--breakpoint-xl) flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="flex items-center space-x-3 rtl:space-x-reverse">
+          <Image src="/logo.png" className="h-8" alt="Flowbite Logo" width={32} height={32} />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">SEOLIM</span>
+        </div>
+        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+          <ul className="flex flex-col text-white p-4 md:p-0 mt-4 font-medium rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+            {Object.keys(pages as PageConfig).map((page) => (
+              <li key={page}>
+                <button
+                  type="button"
+                  onClick={() => onLink(page)}
+                  className="block py-2 px-3 rounded-sm md:bg-transparent md:p-0"
+                  aria-label="nav-link"
+                >
+                  {(pages as PageConfig)[page].title}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-        <ul className="flex flex-col text-white p-4 md:p-0 mt-4 font-medium rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
-          {Object.keys(pages as PageConfig).map((page) => (
-            <li key={page}>
-              <Link href={(pages as PageConfig)[page].path} className="block py-2 px-3 rounded-sm md:bg-transparent md:p-0" aria-current="page">{(pages as PageConfig)[page].title}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  </nav>
-);
+      <Portal>
+        <LoadPage isLoad={isLoad} />
+      </Portal>
+    </nav>
+  );
+};
 
 export default Navbar;
--- client/app/srcs/main/main.page.tsx
+++ client/app/srcs/main/main.page.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import {useRouter} from 'next/router';
 import pages from '../page.config.json';
 import {PageConfig} from '../common/common.dto';
 import FlipCard from './components/main.components.flipcard';
+import Portal from "../common/common.components/common.components.portal";
+import LoadPage from "../common/common.components/common.components.loadPage";
 
 const MainPage: React.FC = () => {
   const router = useRouter();
+  const [isLoad, setIsLoad] = useState<boolean>(false);
+  const onLink = useCallback((page: keyof PageConfig) => {
+    setIsLoad(true);
+    router.push((pages as PageConfig)[page].path);
+  }, [router, setIsLoad]);
   return (
     <div className="bg-cover bg-center min-h-screen w-screen bg-linear-to-tr from-slate-600 to-slate-900 grid place-items-center">
       <div className="grid grid-cols-1 gap-6 xl:grid-cols-4 auto-rows-auto">
         {Object.keys(pages as PageConfig).map((page) => {
-          // eslint-disable-next-line react/jsx-no-useless-fragment
           if (page === 'main') return <React.Fragment key={page} />;
           return (
             <FlipCard
@@ -25,11 +31,14 @@ const MainPage: React.FC = () => {
                   {(pages as PageConfig)[page].description}
                 </p>
             )}
-              onClick={() => router.push((pages as PageConfig)[page].path)}
+              onClick={() => onLink(page)}
             />
           );
         })}
       </div>
+      <Portal>
+        <LoadPage isLoad={isLoad} />
+      </Portal>
     </div>
   );
 };
