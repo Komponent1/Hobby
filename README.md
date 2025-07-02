811e9a9b59a5b0fe26286b59017560cd4d9fa87f
seo2im <seolim6492@gmail.com>
2025-07-02 15:11:50 +0900

    fix: change readme

--- README.md
+++ README.md
@@ -1,36 +1,33 @@
-<h1>
-  <img src="https://blog-seolim.vercel.app/logo.png" alt="logo" width="32px" height="32px"/>
-  모두의 개발
-</h1>
+<div align=center>
+  <a href="https://blog-sage-pi.vercel.app/">
+    <img src="https://avatars.githubusercontent.com/u/73334068?v=4">
+  </a>
+  <h1>
+    <a href="https://blog-sage-pi.vercel.app/">취미 저장소</a>
+  </h1>
+</div>
 
-![img](https://img.shields.io/badge/react-61dafd?style=for-the-badge&logo=REACT&logoColor=white)
-![img](https://img.shields.io/badge/Next.js-000000?style=for-the-badge&logo=Next.js&logoColor=white)
-![img](https://img.shields.io/badge/Docker-2496ED?style=for-the-badge&logo=Docker&logoColor=white)
-
-
-
-### LINK
-
-<a href="https://blog-sage-pi.vercel.app">
-  <img src="https://blog-seolim.vercel.app/logo.png" width="24px" height="24px" />
-  모두의 개발 사이트(blog site link)
+<a href="https://blog-sage-pi.vercel.app/article">
+  <h3>블로그</h3>
 </a>
-
-## Content
-
-### 블로그
 
 개발 게시글 저장소
 
-### 정보 모음
+<a href="https://blog-sage-pi.vercel.app/information">
+  <h3>정보모음</h3>
+</a>
 
-간단한 정보 저장소
+한 문단 개발 정보 저장소
 
-### 게임판
+<a href="https://blog-sage-pi.vercel.app/article">
+  <h3>게임판</h3>
+</a>
 
 웹 게임 저장소
 
-### 스팀 개인 데이터 맵
+<a href="https://blog-sage-pi.vercel.app/article">
+  <h3>스팀 분석</h3>
+</a>
 
 스팀 개인 정보 데이터 정리 서비스
 
--- client/app/srcs/common/common.components/common.components.navbar.tsx
+++ client/app/srcs/common/common.components/common.components.navbar.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from "next/router";
 import pages from '../../page.config.json';
 import {PageConfig} from '../common.dto';
@@ -18,10 +19,12 @@ const Navbar: React.FC = () => {
   return (
     <nav className="bg-slate-900 fixed w-full z-50 top-0 start-0 shadow-md">
       <div className="max-w-(--breakpoint-xl) flex flex-wrap items-center justify-between mx-auto p-4">
-        <div className="flex items-center space-x-3 rtl:space-x-reverse">
-          <Image src="/logo.png" className="h-8" alt="Flowbite Logo" width={32} height={32} />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">SEOLIM</span>
-        </div>
+        <Link href="/">
+          <div className="flex items-center space-x-3 rtl:space-x-reverse">
+            <Image src="/logo.png" className="h-8" alt="Flowbite Logo" width={32} height={32} style={{borderRadius: 32}} />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">SEOLIM</span>
+          </div>
+        </Link>
         <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
           <ul className="flex flex-col text-white p-4 md:p-0 mt-4 font-medium rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
             {Object.keys(pages as PageConfig).map((page) => (
