0fce1fdef9d99e1c885585bdb5c54e784e188a8e
seo2im <seolim6492@gmail.com>
2025-12-25 13:03:54 +0900

    feature: add se;ect

--- client/app/srcs/seolim-ui/component/index.tsx
+++ client/app/srcs/seolim-ui/component/index.tsx
@@ -4,3 +4,4 @@ export { default as HeadBox } from './seolim-ui.component.headBox';
 export { default as UiBox } from './seolim-ui.component.uibox';
 export { default as Sidebar } from './seolim-ui.component.sidebar';
 export { default as PropsTable } from './seolim-ui.component.propsTable';
+export { default as Example } from './seolim-ui.component.example';
